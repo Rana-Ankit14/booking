f364a0275927ac837da23635aec0d86ff5273160
Rana Ankit Singh <38341934+Rana-Ankit14@users.noreply.github.com>
2020-07-20 04:48:21 +0530

    Added jwt token

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-const express = require('express');
 require('dotenv').config()
+const express = require('express');
+const jwt = require('jsonwebtoken')
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/login', (req, res) => {
 
-    console.log(req.body.email);
+
+
+const tokenVerification = (req,res,next) =>{
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null){
+        return res.sendStatus(401);
+    }
+    jwt.verify(token,process.env.AUTH_TOKEN,(err,user)=>{
+        if(err){
+            return res.sendStatus(403);
+        }
+
+        req.user = user;
+        next();
+    });
+};
+
+app.get('/api/book',tokenVerification,(req,res)=>{
     res.sendStatus(200);
 })
+app.post('/api/login', (req, res) => {
+    const user = {
+        id:1,
+        username: 'Ankit',
+        email: 'rana@gmail.com'
+    }
+    jwt.sign({user},process.env.AUTH_TOKEN,(err,token)=>{
+        res.json({token})
+    });
+    // res.redirect('/register');
+});
 
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Server up and running....');
+    console.log('Server up and running on port 5000 .... ');
 })
