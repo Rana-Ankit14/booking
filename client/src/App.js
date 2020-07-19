78475d2eeca56bee18060cc50cfb572ebb4613ba
Rana Ankit Singh <38341934+Rana-Ankit14@users.noreply.github.com>
2020-07-20 03:32:16 +0530

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import './App.css';
 import {Login} from './components/Login'
 import {Register} from './components/Register'
+import {PageNotFound} from "./components/PageNotFound";
 
 function App() {
     return (
-        <div className="App">
-            <Register/>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path={["/","/login"]} component={Login}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/404" component={PageNotFound}/>
+                <Redirect to="/404"/>
+            </Switch>
+        </Router>
     );
 }
 
